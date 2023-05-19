106bd99e3cdd9d3e532ff20afa3f84e1e435fb1b
Emanuel Njiru <emanuelmutuiri@gmail.com>
2023-05-19 06:00:38 +0300

    worked o styling the blog

--- src/Pages/Highlights.jsx
+++ src/Pages/Highlights.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const list = [
     {
@@ -20,17 +22,38 @@ const list = [
 ]
 
 const Highlights = ()=>{
-   
+   const [state, setState] = useState(false)
+
+    const handleClick = ()=>{
+        setState(!state)
+
+    }
+
     return(
-        <div >
-            <h3 style={{textAlign:"center"}}>Highlights</h3>
+        <>
+        <div style={{display:"block", position:"absolute", left:"70%", top:"15%", width:"30%", height:"100%", borderRight:"5px solid", borderBottom :"10px solid #95b4f8", textAlign:"center", boxShadow:"5px 3px 15px #2acfd1"}}>
+            <h3 style={{textDecoration:"overline", paddingBottom:"20px"}} >Highlights</h3>
          {
           list.map((item, index)=>{
-              return <h4 key={index}>{item.title}</h4>
-                 
+              return <Link style={{display: "block", color:"#3a0563", padding:"20px", textDecoration:"none", fontSize:"20px"}} onClick={handleClick} key={index}>{item.title} <br /> </Link> 
+            
           })
         }
+        {
+            state && list.map((item, index)=>{
+                return <p  key={index}>{item.content.slice(0, 50)}</p>
+              
+            })
+        }
         </div>
+
+
+        <div style={{position:"absolute", zIndex:"-1", left:"25%", top:"25%", width:"70%"}}>
+            <img style={{width:"40%"}} src="./vite.svg" alt="" />
+
+        </div>
+        </>
+        
     )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ const Header = ()=>{
     return(
         <>
         <div style={{display:"flex", justifyContent:"space-around", alignItems:"center"}}>
-            <Link to="/about" style={{textDecoration:"none", color:"black", fontSize:"20px"}}><FcAbout />About</Link>
-            <Link to="/home" style={{textDecoration:"none", color:"black", fontSize:"20px"}}>< ImHome />Home</Link>
-            <Link to="/highlights" style={{textDecoration:"none", color:"black", fontSize:"20px"}}><BiAlignJustify />Highlights</Link>           
+            <Link to="/about" style={{textDecoration:"none", color:"black", fontSize:"20px"}}><FcAbout /> About</Link>
+            <Link to="/home" style={{textDecoration:"none", color:"black", fontSize:"20px"}}>< ImHome /> Home</Link>
+            <Link to="/highlights" style={{textDecoration:"none", color:"black", fontSize:"20px"}}><BiAlignJustify /> Highlights</Link>           
         </div>
         <div >
-           <h1 style={{textAlign:"center"}}>Productivity Blog</h1>          
+           <h1 style={{textAlign:"center", color:"#080d87"}}>Productivity Blog</h1>          
         </div>
         </>
         
